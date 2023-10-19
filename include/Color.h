6c7ca229cef5874bd14391a9edbcce05ae6dcfe8
Roberto Garcia <robertoeg97@gmail.com>
2023-10-19 01:25:30 -0500

    added conversion to gamma 2 space when writing color

--- include/Color.h
+++ include/Color.h
@@ -15,9 +15,15 @@ public:
     using Triple<Color>::Triple;    //inherit base class constructors
 
     void write_pixel(std::ostream& out) const {
-        out << static_cast<int>(x() * (max_pixel_val+.999)) << ' '
-            << static_cast<int>(y() * (max_pixel_val+.999)) << ' '
-            << static_cast<int>(z() * (max_pixel_val+.999)) << '\n';
+        Color gamma = get_gamma();
+        out << static_cast<int>(gamma.x() * (max_pixel_val+.999)) << ' '
+            << static_cast<int>(gamma.y() * (max_pixel_val+.999)) << ' '
+            << static_cast<int>(gamma.z() * (max_pixel_val+.999)) << '\n';
+    }
+
+private:
+    Color get_gamma() const {
+        return Color{std::sqrt(m_vec[0]), std::sqrt(m_vec[1]), std::sqrt(m_vec[2])};
     }
 };
 
@@ -31,7 +37,7 @@ public:
         return scale * Color{x(), y(), z()};
     }
 
-    ColorSum operator+=(Color color) {
+    ColorSum operator+=(const Color& color) {
         m_vec[0] += color.x();
         m_vec[1] += color.y();
         m_vec[2] += color.z();
