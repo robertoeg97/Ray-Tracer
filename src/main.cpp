f26ed2cf41ce1b047ecab0f72644faf817b7fa62
Roberto Garcia <robertoeg97@gmail.com>
2023-10-23 19:44:43 -0500

    added null for material (not yet implemented) to allow compilation

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@
 int main() {
     //world info
     HittableList world;
-    world.add(std::make_shared<Sphere>(Vector3D{0, 0, -1}, .5));
-    world.add(std::make_shared<Sphere>(Vector3D{0, -100.5, -1}, 100));
+    world.add(std::make_shared<Sphere>(Vector3D{0, 0, -1}, .5, nullptr));
+    world.add(std::make_shared<Sphere>(Vector3D{0, -100.5, -1}, 100, nullptr));
 
     constexpr float_type aspect_ratio = 16.0/9.0;
     constexpr int image_width = 400;
