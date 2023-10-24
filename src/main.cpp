40fba7facda01b9e090ac90f06212da0cde9f4d2
Roberto Garcia <robertoeg97@gmail.com>
2023-10-24 01:21:08 -0500

    added multiple sphere of different material types to world

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,22 @@
 #include "HittableList.h"
 #include "Sphere.h" 
 #include "Camera.h"
+#include "Material.h"
 
 
 int main() {
     //world info
     HittableList world;
-    world.add(std::make_shared<Sphere>(Vector3D{0, 0, -1}, .5, nullptr));
-    world.add(std::make_shared<Sphere>(Vector3D{0, -100.5, -1}, 100, nullptr));
+
+    auto material_ground = std::make_shared<Lambertian>(Color{.8, .8, 0});
+    auto material_center = std::make_shared<Lambertian>(Color{.7, .3, .3});
+    auto material_left = std::make_shared<Metal>(Color{.8, .8, .8});
+    auto material_right = std::make_shared<Metal>(Color{.8, .6, .2});
+
+    world.add(std::make_shared<Sphere>(Vector3D{0, -100.5, -1}, 100, material_ground));
+    world.add(std::make_shared<Sphere>(Vector3D{0, 0, -1}, .5, material_center));
+    world.add(std::make_shared<Sphere>(Vector3D{-1, 0, -1}, .5, material_left));
+    world.add(std::make_shared<Sphere>(Vector3D{1, 0, -1}, .5, material_right));
 
     constexpr float_type aspect_ratio = 16.0/9.0;
     constexpr int image_width = 400;
